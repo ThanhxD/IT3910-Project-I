560fc328197b4190f3446ad394886cd234d2a0d6
ThanhxD <dangxthanh96@gmail.com>
2017-10-31 22:17:51 +0700

    Add form register

--- src/client/js/homepage.js
+++ src/client/js/homepage.js
@@ -1,5 +1,9 @@
 window.onload = function() {
-    setInterval(runSlider, 5000);
+    setInterval(runSlider, 10000);
+    var btnActionCall = document.querySelector('button.btn-action-call');
+    btnActionCall.onclick = openFormRegister;
+    var btnCloseFormRegister = document.querySelector('div.close-btn');
+    btnCloseFormRegister.onclick = closeFormRegister;
 }
 
 function runSlider() {
@@ -7,4 +11,14 @@ function runSlider() {
     var nextActive = currentActive.nextElementSibling || currentActive.parentNode.firstElementChild;
     currentActive.classList.remove('slide-active');
     nextActive.classList.add('slide-active');
+}
+
+function openFormRegister() {
+    var formContainer = document.querySelector('.form-container');
+    formContainer.classList.remove('hidden');
+}
+
+function closeFormRegister() {
+    var formContainer = document.querySelector('.form-container');
+    formContainer.classList.add('hidden');
 }
