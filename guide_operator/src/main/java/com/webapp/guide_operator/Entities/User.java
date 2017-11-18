fe375eeef615635d237a1360a91503c2d9cfab8c
khaiNb <20151998@student.hust.edu.vn>
2017-11-18 15:33:54 +0700

    New Index.html

--- guide_operator/src/main/java/com/webapp/guide_operator/Entities/User.java
+++ guide_operator/src/main/java/com/webapp/guide_operator/Entities/User.java
@@ -1,13 +1,20 @@
 package com.webapp.guide_operator.Entities;
 
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
-
-import javax.persistence.*;
-import java.io.Serializable;
-
 
 @Entity
 @Table(name = "user")
@@ -49,6 +56,7 @@ public class User implements Serializable {
     @Email
     @Column(name = "email")
     private String email;
+
 
 
 
